823be2f6db373e1a6d121ebebb87bf61d23c10d9
Ali Nauf <naufpsychic@gmail.com>
2021-06-06 01:03:07 +0500

    Inquire Link

--- layouts/partials/Nav.vue
+++ layouts/partials/Nav.vue
@@ -57,13 +57,13 @@
       </div>
       <div class="hidden  md:flex items-center">
         <div class="flex-shrink-0">
-          <button type="button" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gf-main  shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <a href="mailto:greenflora2021@gmail.com" class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gf-main  shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <!-- Heroicon name: solid/plus -->
             <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
             </svg>
             <span>Inquire Now</span>
-          </button>
+          </a>
         </div>
 
       </div>
